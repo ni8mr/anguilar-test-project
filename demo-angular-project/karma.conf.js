f453e634bff0e2f1e5232b47e6ed1697be12d091
ni8mr <nilpaddo@gmail.com>
2021-07-12 17:40:14 +0600

    Initialized the project.

--- demo-angular-project/karma.conf.js
+++ demo-angular-project/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/getting-started-ng5'),
+      dir: require('path').join(__dirname, './coverage/demo-angular-project'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- demo-angular-project/src/app/app.component.ts
+++ demo-angular-project/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'getting-started-ng5';
+  title = 'demo-angular-project';
 }
